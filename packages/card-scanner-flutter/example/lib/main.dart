402f8ed3488b154a73cccf533da125395bec8330
Vartec <misticmvm@gmail.com>
2026-01-23 18:32:58 +0300

    feat(card-scanner): initialize flutter card scanner package

---
+++ packages/card-scanner-flutter/example/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:card_scanner_example/scan_option_configure_widget/scan_option_configure_widget.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:card_scanner/card_scanner.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  CardDetails? _cardDetails;
+  CardScanOptions scanOptions = CardScanOptions(
+    scanCardHolderName: true,
+    // enableDebugLogs: true,
+    validCardsToScanBeforeFinishingScan: 5,
+    possibleCardHolderNamePositions: [
+      CardHolderNameScanPosition.aboveCardNumber,
+    ],
+  );
+
+  Future<void> scanCard() async {
+    var cardDetails = await CardScanner.scanCard(scanOptions: scanOptions);
+    if (!mounted) return;
+    setState(() {
+      _cardDetails = cardDetails;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('card_scanner app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  scanCard();
+                },
+                child: Text('scan card'),
+              ),
+              Text('$_cardDetails'),
+              Expanded(
+                child: OptionConfigureWidget(
+                  initialOptions: scanOptions,
+                  onScanOptionChanged: (newOptions) => scanOptions = newOptions,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
