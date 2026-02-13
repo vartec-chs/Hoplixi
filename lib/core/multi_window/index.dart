de84decb0ed85049ba789b06dc7d63aa3e5bb146
Vartec <misticmvm@gmail.com>
2026-02-14 00:45:02 +0300

    refactor(multi-window): remove unused export for run_sub_window_entry

--- lib/core/multi_window/index.dart
+++ lib/core/multi_window/index.dart
@@ -6,7 +6,6 @@ library;
 
 export 'multi_window_service.dart';
 export 'sub_window_app.dart';
-export '../../run_sub_window_entry.dart';
 export 'sub_window_type.dart';
 export 'window_channel_service.dart';
 export 'window_controller_ext.dart';
